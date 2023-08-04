17a1c81f3cf48dd40096f3a4f1694d8af772cf72
joshhkim02 <joshhkim02@gmail.com>
2023-08-04 15:56:35 -0700

    Added Shift class

---
+++ ShiftLoggerAPI/Models/Shift.cs
@@ -0,0 +1,10 @@
+﻿namespace ShiftLoggerAPI.Models
+{
+    public class Shift
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Date { get; set; } = null!;
+        public string HoursWorked { get; set; } = null!;
+    }
+}

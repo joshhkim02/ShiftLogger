7f0fa159b2829b562fb69d96556cfe76e4fd61bf
joshhkim02 <joshhkim02@gmail.com>
2023-08-04 16:44:11 -0700

    Created Database and updated with InitialMigration

---
+++ ShiftLoggerAPI/Data/ShiftLoggerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftLoggerAPI.Models;
+
+namespace ShiftLoggerAPI.Data
+{
+    public class ShiftLoggerContext : DbContext
+    {
+        public DbSet<Shift> Shifts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\ShiftLoggerDB;Database=ShiftLogger;Trusted_Connection=True");
+        }
+    }
+}
